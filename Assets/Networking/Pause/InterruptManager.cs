04dccf188ea5542bd9ab65f2cfd96264c785130b
Feiyang Li <me@feiyang.dev>
2021-02-19 13:52:45 -0500

    Attempt fix rejoin

--- Assets/Networking/Pause/InterruptManager.cs
+++ Assets/Networking/Pause/InterruptManager.cs
@@ -1,14 +1,18 @@
 using UnityEngine;
 using Photon.Pun;
+using UnityEngine.InputSystem;
 
 public class InterruptManager : IInRoomCallbacks {
     public GameObject PauseWidgetPrefab;
     public Canvas UICanvas;
     private InvitationCodePresenter Presenter;
+    public PlayerInput PlayerInputComponent;
+
+    private GameObject widget;
 
     public override void OnPlayerLeftRoom(Photon.Realtime.Player otherPlayer) {
         Debug.Log("Player left room");
-        GameObject widget = Instantiate(PauseWidgetPrefab);
+        widget = Instantiate(PauseWidgetPrefab);
         widget.transform.SetParent(UICanvas.transform, false);
         RectTransform rect = widget.GetComponent<RectTransform>();
         rect.anchorMin = new Vector2(1, 0);
@@ -16,10 +20,22 @@ public class InterruptManager : IInRoomCallbacks {
         rect.pivot = new Vector2(0.5f, 0.5f);
         rect.offsetMax = new Vector2(10, 20);
         rect.offsetMin = new Vector2(800, 500);
-        
         Presenter = widget.GetComponentInChildren<InvitationCodePresenter>();
         if (Presenter != null) {
             Presenter.Present(PhotonNetwork.CurrentRoom.Name);
         }
+
+        if (PlayerInputComponent != null) {
+            PlayerInputComponent.enabled = false;
+        }
+
+        Time.timeScale = 0;
+    }
+
+    public override void OnPlayerEnteredRoom(Photon.Realtime.Player newPlayer) {
+        Debug.Log("Player joined room");
+        Destroy(widget);
+        PlayerInputComponent.enabled = true;
+        Time.timeScale = 1;
     }
 }
