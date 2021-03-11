ebc23f03bb3041e1dc7970b022de40ecc6229ebc
ZiQi <ziqi.li@mail.mcgill.ca>
2021-03-11 18:07:08 -0500

    add Voice chat system UI

--- Assets/Scripts/VoiceChat/VoiceChatManager.cs
+++ Assets/Scripts/VoiceChat/VoiceChatManager.cs
@@ -1,7 +1,9 @@
+using Photon.Pun;
 using Photon.Voice.PUN;
 using Photon.Voice.Unity;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 /// <summary>
 /// Author: Ziqi Li
@@ -12,10 +14,18 @@ using UnityEngine.InputSystem;
 public class VoiceChatManager : MonoBehaviour {
 
     public Recorder recorder;
+
+    public Sprite TalkIcon;
+    public Sprite MuteIcon;
+    public Image MasterImage;
+    public Image ClientImage;
+
     private PhotonVoiceNetwork punVoiceNetwork;
+    private PhotonView PhotonView;
 
     private void Awake() {
         punVoiceNetwork = PhotonVoiceNetwork.Instance;
+        PhotonView = GetComponent<PhotonView>();
     }
 
     /// <summary>
@@ -26,6 +36,10 @@ public class VoiceChatManager : MonoBehaviour {
     public void OnTalk() {
         Debug.Log("Talk");
         this.recorder.TransmitEnabled = true;
+
+        // set the mic icon using RPC (don't need PhotonView.isMine since both clients have only one VC manager)
+        if (PhotonNetwork.IsMasterClient) PhotonView.RPC("RPC_SetTalkIcon", RpcTarget.All, true);
+        else PhotonView.RPC("RPC_SetTalkIcon", RpcTarget.All, false);
     }
 
     /// <summary>
@@ -36,5 +50,32 @@ public class VoiceChatManager : MonoBehaviour {
     public void OnMute() {
         Debug.Log("Mute");
         this.recorder.TransmitEnabled = false;
+
+        if (PhotonNetwork.IsMasterClient) PhotonView.RPC("RPC_SetMuteIcon", RpcTarget.All, true);
+        else PhotonView.RPC("RPC_SetMuteIcon", RpcTarget.All, false);
+    }
+
+    /// <summary>
+    /// Author: Ziqi Li
+    /// RPC function for setting talking icon
+    /// </summary>
+    /// <param name="isMaster"></param>
+    [PunRPC]
+    private void RPC_SetTalkIcon(bool isMaster)
+    {
+        if (isMaster) MasterImage.sprite = TalkIcon;
+        else ClientImage.sprite = TalkIcon;
+    }
+
+    /// <summary>
+    /// Author: Ziqi Li
+    /// RPC function for setting mute icon
+    /// </summary>
+    /// <param name="isMaster"></param>
+    [PunRPC]
+    private void RPC_SetMuteIcon(bool isMaster)
+    {
+        if (isMaster) MasterImage.sprite = MuteIcon;
+        else ClientImage.sprite = MuteIcon;
     }
 }
