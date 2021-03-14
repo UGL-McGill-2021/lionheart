2e72841bc61ded65d97f80871c07b406c6e1c7a8
ZiQi <ziqi.li@mail.mcgill.ca>
2021-03-14 00:01:01 -0500

    fix player list rpc

--- Assets/Prototype1/TempScripts/GameManager.cs
+++ Assets/Prototype1/TempScripts/GameManager.cs
@@ -11,20 +11,29 @@ public class GameManager : MonoBehaviour {
 
     public List<GameObject> PathPoints = new List<GameObject>();
     public List<GameObject> EnemySpawningPoints = new List<GameObject>();
+
+    // for targeting by enemies
     public List<GameObject> PlayerList = new List<GameObject>();
+    public List<GameObject> TurretTargets = new List<GameObject>();
 
     public bool Loading = false;
     public SceneLoader SceneLoader;
 
-    // Start is called before the first frame update
-    void Awake() {
+    private PhotonView PhotonView;
 
+    // Start is called before the first frame update
+    void Start() {
+
+        PhotonView = GetComponent<PhotonView>();
         GameObject Player, Player2;
 
         if (PhotonNetwork.IsMasterClient) // 2
         {
+            // generate player1
             Player = PhotonNetwork.Instantiate("Playerv2", new Vector3(0, 4f, 0), Quaternion.identity);
-            PlayerList.Add(Player);
+            int ViewId = Player.gameObject.GetComponent<PhotonView>().ViewID;
+            PhotonView.RPC("RPC_addPlayer", RpcTarget.All, ViewId);  // use RPC call to add player
+
             // Generate moving platforms
             GameObject platform = PhotonNetwork.Instantiate("MPlatformRB",
                 PathPoints[0].transform.position,
@@ -79,14 +88,13 @@ public class GameManager : MonoBehaviour {
                  PathPoints[7].transform.position,
                 Quaternion.identity);
             obj.GetComponent<SpiritWall>().SetIsOneWay(false);
-
-
-
         } 
         else 
         {
+            // generate player2
             Player2 = PhotonNetwork.Instantiate("Playerv2", new Vector3(4, 1.25f, 0), Quaternion.identity);
-            PlayerList.Add(Player2);
+            int ViewId = Player2.gameObject.GetComponent<PhotonView>().ViewID;
+            PhotonView.RPC("RPC_addPlayer", RpcTarget.All, ViewId);  // use RPC call to add player
         }
     }
 
@@ -98,6 +106,20 @@ public class GameManager : MonoBehaviour {
             SceneLoader.LoadPhotonSceneWithName("SampleScene");
             Loading = false;
         }
+    }
+
+    /// <summary>
+    /// Author: Ziqi Li
+    /// RPC function for adding player to player list using the player PhotonViewID
+    /// since we cannot pass gameobject directly using Photon
+    /// </summary>
+    /// <param name="playerViewID">The PhotonViewID of the gameobject</param>
+    [PunRPC]
+    void RPC_addPlayer(int playerViewID)
+    {
+        GameObject player = PhotonView.Find(playerViewID).gameObject;
+        PlayerList.Add(player);
+        TurretTargets.Add(player);
     }
 
 }
