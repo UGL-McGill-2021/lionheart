c609a81a050eac5b19b106faaca171cf2585c9c8
Feiyang Li <me@feiyang.dev>
2021-04-02 17:10:39 -0400

    Fixed the edge problem between temp platform and enemies

--- Assets/Scripts/Gameplay Objects/TempPlatformStateTrigger.cs
+++ Assets/Scripts/Gameplay Objects/TempPlatformStateTrigger.cs
@@ -9,22 +9,24 @@ public class TempPlatformStateTrigger : MonoBehaviour {
     public TempPlatform platform;
     public bool PlatformAppears = true;
 
-    private List<EnemyCombatManager> OverlappingNPCs = new List<EnemyCombatManager>();
+    public List<EnemyCombatManager> OverlappingNPCs = new List<EnemyCombatManager>();
 
     private void Start() {
         platform.OnPlatformStateChanged += UpdateNPCControl;
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.tag == "Enemy") {
-            EnemyCombatManager _enemy = other.gameObject.GetComponent<EnemyCombatManager>();
+        
+        if (other.gameObject.tag == "EnemyTempPlatformProbe") {
+            EnemyCombatManager _enemy = other.gameObject.transform.parent.GetComponent<EnemyCombatManager>();
             if (_enemy != null) {
+                Debug.Log("Probed enemy combat manager");
                 if (!PlatformAppears) {
                     NavMeshAgent _agent = _enemy.gameObject.GetComponent<NavMeshAgent>();
                     Rigidbody _body = _enemy.gameObject.GetComponent<Rigidbody>();
                     if (_agent != null && _body != null) {
                         if (_agent.enabled && _body.isKinematic) {
-                            //_enemy.TriggerGivePhysControlOnAll(true);
+                            _enemy.TriggerGivePhysControlOnAll(true);
                         }
                     }
                 }
@@ -32,15 +34,18 @@ public class TempPlatformStateTrigger : MonoBehaviour {
                 OverlappingNPCs.Add(_enemy);
             }
         }
+        
     }
 
     private void OnTriggerExit(Collider other) {
-        if (other.gameObject.tag == "Enemy") {
-            EnemyCombatManager _enemy = other.gameObject.GetComponent<EnemyCombatManager>();
+        
+        if (other.gameObject.tag == "EnemyTempPlatformProbe") {
+            EnemyCombatManager _enemy = other.gameObject.transform.parent.GetComponent<EnemyCombatManager>();
             if (_enemy != null) {
                 OverlappingNPCs.Remove(_enemy);
             }
         }
+        
     }
 
     private void UpdateNPCControl(bool PlatformAppears) {
@@ -56,6 +61,7 @@ public class TempPlatformStateTrigger : MonoBehaviour {
                 manager.TriggerGivePhysControlOnAll(true);
             }
         }
+
     }
 
     private void OnDestroy() {
