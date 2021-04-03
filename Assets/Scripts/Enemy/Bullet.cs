ef555bdfb8d91e81e610ea85a1f55843d31ba815
Feiyang Li <me@feiyang.dev>
2021-04-03 09:08:37 -0400

    Fixed Bullet Hit Direction

--- Assets/Scripts/Enemy/Bullet.cs
+++ Assets/Scripts/Enemy/Bullet.cs
@@ -6,7 +6,12 @@ public class Bullet : MonoBehaviour {
 
     public float BulletAttackTimeSpan = 0.3f;
     public float Force = 500;
+    [HideInInspector]
     public GameObject owner;
+    [HideInInspector]
+    private Vector3 prevPosition;
+    [HideInInspector]
+    public Vector3 Forward;
 
     private void Awake() {
         StartCoroutine(DestroyDelay());
@@ -20,8 +25,8 @@ public class Bullet : MonoBehaviour {
         if (Other.gameObject.tag == "Player") {
             PlayerCombatManager _playerCombatManager = Other.gameObject.GetComponent<PlayerCombatManager>();
             if (_playerCombatManager != null) {
-                Vector3 _AttackVector = (Other.transform.position - this.transform.position).normalized * Force;
-                _playerCombatManager.ReceivePlayerAttack(_AttackVector, BulletAttackTimeSpan);
+                //Vector3 _AttackVector = (Other.transform.position - this.transform.position).normalized * Force;
+                _playerCombatManager.ReceivePlayerAttack(Forward * Force, BulletAttackTimeSpan);
             }
 
             Destroy(this.gameObject);
@@ -29,12 +34,17 @@ public class Bullet : MonoBehaviour {
         } else if (Other.gameObject.tag == "Enemy" && Other.gameObject != owner) {
             EnemyCombatManager _enemyCombatManager = Other.gameObject.GetComponent<EnemyCombatManager>();
             if (_enemyCombatManager != null) {
-                Vector3 _AttackVector = (Other.transform.position - this.transform.position).normalized * Force;
-                _enemyCombatManager.ReceiveAttack(_AttackVector, BulletAttackTimeSpan);
+                //Vector3 _AttackVector = (Other.transform.position - this.transform.position).normalized * Force;
+                _enemyCombatManager.ReceiveAttack(Forward * Force, BulletAttackTimeSpan);
             }
 
             Destroy(this.gameObject);
         }
+    }
+
+    public void FixedUpdate() {
+        Forward = (transform.position - prevPosition).normalized;
+        prevPosition = transform.position;
     }
 
     IEnumerator DestroyDelay() {
