aa2aae1bc78f831e56a69c8388592d4c1839163d
dpad46 <46.daniel.46@gmail.com>
2021-04-06 16:03:14 -0700

    Changed Final Checkpoint Behaviour

--- Assets/Scripts/Checkpoint/Checkpoint.cs
+++ Assets/Scripts/Checkpoint/Checkpoint.cs
@@ -11,8 +11,10 @@ public class Checkpoint : MonoBehaviour
     public List<Transform> SpawnPoints;
 
     public CheckpointManager CheckpointMan;
-    private GameObject LastPlayerEntered;
+    public bool IsFinalCheckpoint;
 
+    private GameObject LastPlayerEntered;
+    private HashSet<GameObject> PlayersInArea;
 
     void Awake()
     {
@@ -21,25 +23,52 @@ public class Checkpoint : MonoBehaviour
 
     private void OnTriggerEnter(Collider Other)
     {
-        if (Other.tag == "Player")
+        if (IsFinalCheckpoint)
         {
-            if (!LastPlayerEntered)
+            if (Other.tag == "Player")
             {
-                LastPlayerEntered = Other.gameObject;
-            }
-            else
-            {
-                if (LastPlayerEntered != Other.gameObject)
+                PlayersInArea.Add(Other.gameObject);
+                if (PlayersInArea.Count == 2)
                 {
-                    CheckpointMan.SetCheckpoint(this);
-                    CheckpointMan.CheckpointCompleted(this);
-                    GetComponent<BoxCollider>().enabled = false;
-                    GetComponent<MeshRenderer>().enabled = false;
+                    CheckpointMan.FinalCheckpointComplete();
+                }
+            }
+        } else {
+            if (Other.tag == "Player")
+            {
+                if (!LastPlayerEntered)
+                {
+                    LastPlayerEntered = Other.gameObject;
+                }
+                else
+                {
+                    if (LastPlayerEntered != Other.gameObject)
+                    {
+                        CheckpointMan.SetCheckpoint(this);
+                        CheckpointCompleted();
+                    }
                 }
             }
         }
-        
     }
+
+    private void OnTriggerExit(Collider Other)
+    {
+        if (IsFinalCheckpoint) {
+            if (Other.tag == "Player")
+            {
+                PlayersInArea.Remove(Other.gameObject);
+            }
+        }
+    }
+
+
+    private void CheckpointCompleted()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        GetComponent<MeshRenderer>().enabled = false;
+    }
+
 
     //used to help prevent players from being spawned in the same position 
     public Transform GetSpawnPoint(bool IsMaster)
