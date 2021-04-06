9319abedc9d2e88ca0fb0e3ec90535c145cfe046
ZiQi <ziqi.li@mail.mcgill.ca>
2021-04-06 01:22:48 -0400

    update UI and fix disconnection bug

--- Assets/Scripts/Menu/PlayerGameSettings.cs
+++ Assets/Scripts/Menu/PlayerGameSettings.cs
@@ -8,5 +8,7 @@ public static class PlayerGameSettings
 {
     public static bool IsVibrationOn = true;
     public static float AudioVolume = 0.5f;  // default volume is 0.5
+    public static float SFXVolume = 0.5f;  // sfx
+    public static float VCVolume = 0.5f;  // voice chat
     public static bool IsInGameMenuOpened = false;
 }
