3ccb5c418caa6a2993bbdd943b343f7f0d8e3503
ZiQi <ziqi.li@mail.mcgill.ca>
2021-07-01 22:01:28 -0400

    prototype of progress system

---
+++ Assets/Scripts/ProgressSystem/PlayerData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// Author: Ziqi Li
+/// A class represent the player data using for ProgressSystem
+/// </summary>
+public class PlayerData
+{
+    private HashSet<string> unlockedLevels;
+
+    public PlayerData()
+    {
+        unlockedLevels = new HashSet<string>();
+    }
+
+    /// <summary>
+    /// Author: Ziqi Li
+    /// Function to unlock level in the current PlayerData
+    /// </summary>
+    /// <param name="levelName"></param>
+    public void UnlockLevel(string levelName)
+    {
+        unlockedLevels.Add(levelName);
+    }
+
+
+}
---
+++ Assets/Scripts/ProgressSystem/ProgressSystem.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+/// <summary>
+/// Author: Ziqi Li
+/// A static class for the saving&loading system
+/// </summary>
+public static class ProgressSystem
+{
+    // the PlayerData instance for the current player
+    private static PlayerData currentPlayerData = null;
+
+    /// <summary>
+    /// Author: Ziqi Li
+    /// Function to update the PlayerData and update the corresponding file
+    /// </summary>
+    public static void SavePlayerData()
+    {
+        BinaryFormatter fileFormatter = new BinaryFormatter();
+        string filePath = Application.persistentDataPath + "/BesunderPlayerData.data";
+
+        // create a file, it will overwrite existing file if existed
+        FileStream stream = new FileStream(filePath, FileMode.Create); 
+
+        fileFormatter.Serialize(stream, currentPlayerData);  // convert the PlayerData to binary stream
+        stream.Close();
+    } 
+
+
+    
+}
