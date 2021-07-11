be1bb1a1acaa29b3d3db712ab4e20202002af5ed
ZiQi <ziqi.li@mail.mcgill.ca>
2021-07-11 00:16:22 -0400

    complete scripts

--- Assets/Scripts/ProgressSystem/ProgressSystem.cs
+++ Assets/Scripts/ProgressSystem/ProgressSystem.cs
@@ -19,16 +19,67 @@ public static class ProgressSystem
     /// </summary>
     public static void SavePlayerData()
     {
-        BinaryFormatter fileFormatter = new BinaryFormatter();
-        string filePath = Application.persistentDataPath + "/BesunderPlayerData.data";
+        if(currentPlayerData != null)
+        {
+            BinaryFormatter fileFormatter = new BinaryFormatter();
+            string filePath = Application.persistentDataPath + "/BesunderProgressData.data";
 
-        // create a file, it will overwrite existing file if existed
-        FileStream stream = new FileStream(filePath, FileMode.Create); 
+            // create a file, it will overwrite existing file if existed
+            FileStream stream = new FileStream(filePath, FileMode.Create);
 
-        fileFormatter.Serialize(stream, currentPlayerData);  // convert the PlayerData to binary stream
-        stream.Close();
-    } 
+            fileFormatter.Serialize(stream, currentPlayerData);  // convert the PlayerData to binary stream
+            stream.Close();
+        }
+        else
+        {
+            throw new System.Exception("The current player data is null, please load the data first");
+        }
+    }
+
+    /// <summary>
+    /// Author: Ziqi Li
+    /// Function to read the PlayerData from the saving file and return it
+    /// Also update it to the currentPlayerData field
+    /// </summary>
+    public static PlayerData LoadPlayerData()
+    {
+        string filePath = Application.persistentDataPath + "/BesunderProgressData.data";
+
+        // if the saving file exists
+        if (File.Exists(filePath))
+        {
+            BinaryFormatter fileFormatter = new BinaryFormatter();
+            // create a file, it will overwrite existing file if existed
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+            PlayerData data = (PlayerData) fileFormatter.Deserialize(stream);  // convert the PlayerData to binary stream
+            stream.Close();
+
+            currentPlayerData = data;
+            return currentPlayerData;
+        }
+        else
+        {
+            currentPlayerData = new PlayerData();
+            return currentPlayerData;
+        }
+    }
+
+    /// <summary>
+    /// Author: Ziqi Li
+    /// Function to get the current player data
+    /// </summary>
+    public static PlayerData GetCurrentPlayerData()
+    {
+        if (currentPlayerData != null)
+        {
+            return currentPlayerData;
+        }
+        else
+        {
+            throw new System.Exception("The current player data is null, please load the data first");
+        }
+    }
 
 
-    
+
 }
